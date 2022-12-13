ad32f44f0c703d94626288005521981ae190ddfe
Ivan <ivangybskiy03@gmail.com>
2022-12-13 18:17:36 +0300

    +fonts
    +fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import '@/styles/App.scss'
+import '@/styles/fonts.scss'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-       Hello_world
-       
+       Обувь
+
+
     </div>
   )
 }
